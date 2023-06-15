ef29f32e3a80d7407660e6c38ff57904eee2ad27
Lucas Chagas <lucas.chagas@icomp.ufam.edu.br>
2023-06-15 14:55:38 -0400

    a

--- app/src/main/java/com/wit/example/MainActivity.java
+++ app/src/main/java/com/wit/example/MainActivity.java
@@ -3,12 +3,12 @@ package com.wit.example;
 import static java.lang.Thread.sleep;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
@@ -29,14 +29,17 @@ import java.util.List;
 import java.lang.*;
 public class MainActivity extends AppCompatActivity implements IBluetoothFoundObserver, IBwt901bleRecordObserver {
     private static final String TAG = MainActivity.class.getSimpleName();
-
     private List<Bwt901ble> bwt901bleList = new ArrayList<>();
 
     private boolean destroyed = true;
     private boolean writeOnSensorData = false;
-    private Button writeSensorDataButton = null;
 
+    private boolean isCalibrating = false;
+    private Button startCalibrationButton = null;
+    private Button writeSensorDataButton = null;
+    private Button rotulateButton = null;
     private String writeContent = "";
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-YYYY HH:mm:ss:SSS ");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,14 +63,12 @@ public class MainActivity extends AppCompatActivity implements IBluetoothFoundOb
             handleAppliedCalibration();
         });
 
-        Button startFieldCalibrationButton = findViewById(R.id.startFieldCalibrationButton);
-        startFieldCalibrationButton.setOnClickListener((v) -> {
-            handleStartFieldCalibration();
-        });
-
-        Button endFieldCalibrationButton = findViewById(R.id.endFieldCalibrationButton);
-        endFieldCalibrationButton.setOnClickListener((v) -> {
-            handleEndFieldCalibration();
+        startCalibrationButton = findViewById(R.id.startFieldCalibrationButton);
+        startCalibrationButton.setOnClickListener((v) -> {
+            if (!isCalibrating)
+                handleStartFieldCalibration();
+            else
+                handleEndFieldCalibration();
         });
 
         Button readReg03Button = findViewById(R.id.readReg03Button);
@@ -79,6 +80,8 @@ public class MainActivity extends AppCompatActivity implements IBluetoothFoundOb
         writeSensorDataButton.setOnClickListener((v) -> {
             handleWriteSensorDataButton();
         });
+
+        rotulateButton = findViewById(R.id.rotulateButton);
 
         Thread thread = new Thread(this::refreshDataTh);
         destroyed = false;
@@ -205,29 +208,32 @@ public class MainActivity extends AppCompatActivity implements IBluetoothFoundOb
         builder.append("angleZ,");
         builder.append("hX,");
         builder.append("hY,");
-        builder.append("hZ\n");
+        builder.append("hZ,");
+        builder.append("tag\n");
 
         return builder.toString();
     }
 
-    private String buildSensorData(Bwt901ble bwt901ble, int i) {
+    private String buildSensorData(Bwt901ble bwt901ble) {
         StringBuilder builder = new StringBuilder();
 
+        Date date = new Date();
+        String stringDate = dateFormat.format(date);
 
-        //builder.append(currentTime).append(",");
-        builder.append(i).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.AccX).replace(",",".")).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.AccY).replace(",",".")).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.AccZ).replace(",",".")).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.AsX).replace(",",".")).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.AsY).replace(",",".")).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.AsZ).replace(",",".")).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.AngleX).replace(",",".")).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.AngleY).replace(",",".")).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.AngleZ).replace(",",".")).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.HX).replace(",",".")).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.HY).replace(",",".")).append(",");
-        builder.append(bwt901ble.getDeviceData(WitSensorKey.HZ).replace(",",".")).append("\n");
+        builder.append(stringDate).append(",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.AccX)).append("\",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.AccY)).append("\",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.AccZ)).append("\",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.AsX)).append("\",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.AsY)).append("\",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.AsZ)).append("\",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.AngleX)).append("\",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.AngleY)).append("\",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.AngleZ)).append("\",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.HX)).append("\",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.HY)).append("\",\"");
+        builder.append(bwt901ble.getDeviceData(WitSensorKey.HZ)).append("\",");
+        builder.append(rotulateButton.isPressed() ? "1" : "0").append("\n");
 
         return builder.toString();
     }
@@ -277,11 +283,9 @@ public class MainActivity extends AppCompatActivity implements IBluetoothFoundOb
                 for (int i = 0; i < bwt901bleList.size(); i++) {
                     bwt901ble = bwt901bleList.get(i);
                 }
-                
-                int i = 1;
+
                 while (writeOnSensorData) {
-                    writeContent += buildSensorData(bwt901ble, i);
-                    i += 1;
+                    writeContent += buildSensorData(bwt901ble);
                     try {
                         sleep(100);
                     } catch (InterruptedException e) {
@@ -302,6 +306,11 @@ public class MainActivity extends AppCompatActivity implements IBluetoothFoundOb
     }
 
     private void handleAppliedCalibration() {
+        if (isCalibrating) {
+            Toast.makeText(this, "Termine a calibração antes de aplicar.", Toast.LENGTH_LONG)
+                    .show();
+        }
+
         for (int i = 0; i < bwt901bleList.size(); i++) {
             Bwt901ble bwt901ble = bwt901bleList.get(i);
             // unlock register
@@ -309,10 +318,10 @@ public class MainActivity extends AppCompatActivity implements IBluetoothFoundOb
             // send command
             bwt901ble.appliedCalibration();
         }
-        Toast.makeText(this, "OK", Toast.LENGTH_LONG).show();
     }
 
     private void handleStartFieldCalibration() {
+        isCalibrating = true;
         for (int i = 0; i < bwt901bleList.size(); i++) {
             Bwt901ble bwt901ble = bwt901bleList.get(i);
             // unlock register
@@ -320,10 +329,12 @@ public class MainActivity extends AppCompatActivity implements IBluetoothFoundOb
             // send command
             bwt901ble.startFieldCalibration();
         }
-        Toast.makeText(this, "OK", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Calibrando.", Toast.LENGTH_LONG).show();
+        startCalibrationButton.setText(R.string.End_field_calibration);
     }
 
     private void handleEndFieldCalibration() {
+        isCalibrating = false;
         for (int i = 0; i < bwt901bleList.size(); i++) {
             Bwt901ble bwt901ble = bwt901bleList.get(i);
             // unlock register
@@ -331,7 +342,8 @@ public class MainActivity extends AppCompatActivity implements IBluetoothFoundOb
             // send command
             bwt901ble.endFieldCalibration();
         }
-        Toast.makeText(this, "OK", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Calibrado.", Toast.LENGTH_LONG).show();
+        startCalibrationButton.setText(R.string.Start_field_calibration);
     }
 
     private void handleReadReg03() {
